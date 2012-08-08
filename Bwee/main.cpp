898bd1fa64e3298ba62c3a7517a7c4d706e15ee1
Fabian Ponce <pfabian@macbook-pro-puresend.(none)>
2012-08-08 14:17:00 -0400

    Initial Commit

---
+++ Bwee/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Bwee
+//
+//  Created by Fabian Ponce on 8/8/12.
+//  Copyright (c) 2012 PureSend. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
